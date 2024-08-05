df8f521404a5f42bcdf780f0d5a1c01058f6ce99
kamlesh <kgurjar1947@gmail.com>
2024-08-05 21:02:53 +0530

    mailchimp

--- app/Http/Controllers/EnqController.php
+++ app/Http/Controllers/EnqController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\EnqMail;
 use App\Mail\contactMail;
 use App\Models\Enquiry;
+use Spatie\Newsletter\NewsletterFacade as Newsletter;
 
 class EnqController extends Controller
 {
@@ -28,6 +29,24 @@ class EnqController extends Controller
             // Save the new Country entry
             $enq->save();
 
+
+            // Subscribe the user to the Mailchimp list
+            Newsletter::subscribe($request->get('gmail'), [
+                'FNAME' => $request->get('f_name'),
+                'LNAME' => $request->get('l_name'),
+            ]);
+
+            // Send an email notification to the admin
+            Mail::raw(
+                "New client submission:\n\nName: {$request->get('f_name')} {$request->get('l_name')}\nEmail: {$request->get('gmail')}\nPhone: {$request->get('phone')}\nMessage: {$request->get('description')}", 
+                function ($message) {
+                    $message->to('bsirisha511@gmail.com')
+                            ->subject('New Client Submission');
+                }
+            );
+
+          
+
         return redirect()->back()->with('success','Request Sent successfully.');
     }
     
@@ -47,7 +66,14 @@ class EnqController extends Controller
     
         // Save the new Country entry
         $enq->save();
-      
+
+        Newsletter::subscribe($request->email, ['full_name' => $request->name]);
+
+        // Send an email to the admin
+        Mail::raw("New client submission:\n\nName: {$request->name}\nEmail: {$request->email}\nMessage: {$request->message}", function ($message) {
+            $message->to('bsirisha511@gmail.com')
+                    ->subject('New Client Submission');
+        });
 
         return redirect()->back()->with('success','Request Sent successfully.');
     }
